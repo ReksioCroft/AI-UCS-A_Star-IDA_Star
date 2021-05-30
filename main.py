cfd1766adac77d1c9c06f419bb8f3baa150a6986
Octavian-Florin  Staicu <octavian-florin.staicu@my.fmi.unibuc.ro>
2021-05-30 15:51:21 +0300

    Added memory counter Added inadmissible heuristic THe generated successors are now retained in the memory

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from queue import PriorityQueue
 
 
 class StareGraph:
-    def __init__(self, matrix, robinet, canal, val=0, tata=None, msg="\n"):
+    nr_aflate_in_memorie = 0
+
+    def __init__(self, matrix, robinet, canal, timp_gasire=0.0, val=0, tata=None, poz_modificat=None):
         self.matrix = copy.deepcopy(matrix)
         self.robinet = robinet
         self.canal = canal
         self.tata = tata
         self.val = val
-        self.msg = msg
+        self.poz_modificat = poz_modificat
+        self.timp_gasire = timp_gasire
+        self.succesori = None
 
     def __lt__(self, other):
         return self.val < other.val
@@ -32,22 +36,25 @@ class StareGraph:
 
     @classmethod
     def generare_succesori(cls, stare_curenta):
-        l_succesori = []
+        if stare_curenta.succesori is None:
+            l_succesori = []
 
-        def fill_succesori(matrix, poz):
-            if matrix[poz[0]][poz[1]] == '#':
-                m_aux = copy.deepcopy(stare_curenta.matrix)
-                m_aux[poz[0]][poz[1]] = 'o'
-                nod = StareGraph(matrix=m_aux, robinet=stare_curenta.robinet, canal=stare_curenta.canal, val=stare_curenta.val + StareGraph.calc_val_vecini(m_aux, poz), tata=stare_curenta, msg="Eliminam obstacolul de pe linia " + str(poz[0]) + ", coloana " + str(poz[1]) + '\n')
-                if nod not in l_succesori:
-                    l_succesori.append(nod)
-            elif matrix[poz[0]][poz[1]] == 'o':
-                matrix[poz[0]][poz[1]] = '*'
-                for i in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                    fill_succesori(matrix, (poz[0] + i[0], poz[1] + i[1]))
+            def fill_succesori(matrix, poz):
+                if matrix[poz[0]][poz[1]] == '#':
+                    m_aux = copy.deepcopy(stare_curenta.matrix)
+                    m_aux[poz[0]][poz[1]] = 'o'
+                    nod = StareGraph(matrix=m_aux, robinet=stare_curenta.robinet, canal=stare_curenta.canal, val=stare_curenta.val + StareGraph.calc_val_vecini(m_aux, poz), tata=stare_curenta, poz_modificat=poz, timp_gasire=time.time() - Graph.t0)
+                    if nod not in l_succesori:
+                        l_succesori.append(nod)
+                elif matrix[poz[0]][poz[1]] == 'o':
+                    matrix[poz[0]][poz[1]] = '*'
+                    for i in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                        fill_succesori(matrix, (poz[0] + i[0], poz[1] + i[1]))
 
-        fill_succesori(copy.deepcopy(stare_curenta.matrix), stare_curenta.robinet)
-        return l_succesori
+            fill_succesori(copy.deepcopy(stare_curenta.matrix), stare_curenta.robinet)
+            stare_curenta.succesori = l_succesori
+            cls.nr_aflate_in_memorie += len(l_succesori)
+        return stare_curenta.succesori
 
     @classmethod
     def is_final_state(cls, state):
@@ -60,7 +67,10 @@ class StareGraph:
 
 
 class Graph:
+    t0 = time.time()
+
     def __init__(self, fin, fout):
+
         def initial_matrix():
             matrix = []
             for i in fin:  # vom borda matricea cu None
@@ -77,7 +87,7 @@ class Graph:
 
         robinet = tuple([int(i) for i in fin.readline().split()])  # matrice indexata de la 1 pt bordare
         canal = tuple([int(i) for i in fin.readline().split()])
-        self.stare0 = StareGraph(matrix=initial_matrix(), robinet=robinet, canal=canal, msg="Configuratie Initiala\n")  # construim starea inițială
+        self.stare0 = StareGraph(matrix=initial_matrix(), robinet=robinet, canal=canal)  # construim starea inițială
         self.fout = fout
 
     @classmethod
@@ -110,24 +120,38 @@ class Graph:
                         if j.val < best_value:
                             best_value = j.val
             return best_value
+        elif euristica == "distanta_manhattan_neadmisibila":
+            if current_state.poz_modificat is not None:
+                return current_state.val + abs(current_state.poz_modificat[0] - current_state.canal[0]) + abs(current_state.poz_modificat[1] - current_state.canal[1])
+            else:
+                return current_state.val
         else:
             raise Exception("Eurisitca necunoscuta")
 
     def afis_drum(self, state, co):
         if state.tata is not None:
             self.afis_drum(state.tata, co)
+        else:
+            self.fout.write("Nr de noduri aflate in memorie dupa generarea acestei sol: {}\n".format(str(StareGraph.nr_aflate_in_memorie)))
+
         co[0] += 1
-        self.fout.write(str(co[0]) + ') ' + state.msg)
+        self.fout.write(str(co[0]) + ') ')
+        if state.poz_modificat is not None:
+            self.fout.write("Eliminam obstacolul de pe linia " + str(state.poz_modificat[0]) + ", coloana " + str(state.poz_modificat[1]) + '\n')
+        else:
+            self.fout.write("Configuratie Initiala\n")
         m = copy.deepcopy(state.matrix)
         Graph.fill(m, state.robinet)
         for i in m[1:len(m) - 1]:  # excludem bordarea
             self.fout.write(str(i[1:len(i) - 1]) + '\n')
-        self.fout.write("Cost actual: {}\n".format(state.val))
+        self.fout.write("Lungime intermediara drum: {}\n".format(str(co[0])))
+        self.fout.write("Cost actual: {}\n".format(str(state.val)))
+        self.fout.write("Timp gasire: {}s\n".format(str(round(state.timp_gasire, 5))))
         self.fout.write('...........\n')
 
     def ucs(self, nr_solutii=1, timeout=60):
         state = self.stare0
-        t0 = time.time()
+        Graph.t0 = time.time()
         pq = PriorityQueue()
         pq.put((0, state))
         co_solutii = 0
@@ -146,7 +170,7 @@ class Graph:
                     continue
                 else:
                     break
-            if time.time() - t0 > timeout:
+            if time.time() - Graph.t0 > timeout:
                 self.fout.write("Timeout...\n")
                 break
             for nextState in StareGraph.generare_succesori(state):  # altfel, parcurg succesorii
@@ -157,7 +181,7 @@ class Graph:
     def a_star_optimizat(self, euristica="peste_2_pasi", timeout=60):
         self.fout.write("===========\n")
         state = self.stare0
-        t0 = time.time()
+        Graph.t0 = time.time()
         q = PriorityQueue()
         q.put((Graph.calc_cost_till_end(euristica=euristica, current_state=state), state))
         closed = []
@@ -170,7 +194,7 @@ class Graph:
                 self.fout.write("AStar Optim (euristica {}):\n".format(euristica))
                 self.afis_drum(state, [0])
                 break
-            if time.time() - t0 > timeout:
+            if time.time() - Graph.t0 > timeout:
                 self.fout.write("Timeout...\n")
                 break
             for nextState in StareGraph.generare_succesori(state):  # altfel, parcurg succesorii
@@ -185,7 +209,7 @@ class Graph:
         state = self.stare0
         q = PriorityQueue()
         co_solutii = 0
-        t0 = time.time()
+        Graph.t0 = time.time()
         stari_finale = []
         q.put((Graph.calc_cost_till_end(euristica=euristica, current_state=state), state))
         while not q.empty():
@@ -200,7 +224,7 @@ class Graph:
                     continue
                 else:
                     break
-            if time.time() - t0 > timeout:
+            if time.time() - Graph.t0 > timeout:
                 self.fout.write("Timeout...\n")
                 break
             for nextState in StareGraph.generare_succesori(state):  # altfel, parcurg succesorii
@@ -215,12 +239,16 @@ class Graph:
             elif StareGraph.is_final_state(stare):
                 if stare not in stari_finale:
                     stari_finale.append(stare)
+                    self.fout.write("IDA Star {} (euristica {}):\n".format(len(stari_finale), euristica))
+                    self.fout.write("Nr calculate dupa aceasta solutie: {}\n".format(str(recursie_ida_star.co_noduri_calculate)))
+                    self.afis_drum(state=stare, co=[0])
                 return float('+inf')
-            elif len(stari_finale) < nr_solutii and time.time() - t0 <= timeout:
+            elif len(stari_finale) < nr_solutii and time.time() - Graph.t0 <= timeout:
                 lista_succesori = StareGraph.generare_succesori(stare)
+                recursie_ida_star.co_noduri_calculate += len(lista_succesori)
                 best_cost_recursie = float('+inf')
                 for stare_urmatoare in lista_succesori:
-                    if len(stari_finale) >= nr_solutii or time.time() - t0 > timeout:
+                    if len(stari_finale) >= nr_solutii or time.time() - Graph.t0 > timeout:
                         return cost_euristic_stare_curenta
                     cost_nou_recursie = recursie_ida_star(Graph.calc_cost_till_end(current_state=stare_urmatoare, euristica=euristica), stare_urmatoare)
                     if cost_nou_recursie < best_cost_recursie:
@@ -230,21 +258,17 @@ class Graph:
                 return cost_euristic_stare_curenta
 
         self.fout.write("===========\n")
-        t0 = time.time()
+        Graph.t0 = time.time()
         stari_finale = []
         cost_stare0 = Graph.calc_cost_till_end(euristica=euristica, current_state=self.stare0)
         limita_cost_recursie = cost_stare0
+        recursie_ida_star.co_noduri_calculate = 0
+
         while len(stari_finale) < nr_solutii:
             limita_cost_recursie = recursie_ida_star(cost_stare0, self.stare0)
-            if time.time() - t0 > timeout:
+            if time.time() - Graph.t0 > timeout:
+                self.fout.write("Timeout...\n")
                 break
-
-        for i in range(min(nr_solutii, len(stari_finale))):  # range(min(nr_solutii, len(stari_finale))):
-            self.fout.write("IDA Star {} (euristica {}):\n".format(i + 1, euristica))
-            self.afis_drum(state=stari_finale[i], co=[0])
-
-        if time.time() - t0 > timeout:
-            self.fout.write("Timeout...\n")
 
 
 if __name__ == "__main__":
@@ -257,28 +281,31 @@ if __name__ == "__main__":
         fout = open(outputPath + inputFile + '.out', 'w')
         graph = Graph(fin, fout)
 
-        # print("ucs")
-        # graph.ucs(nr_solutii=nSol, timeout=timeout)
-        #
-        # print("a star")
-        # graph.a_star(euristica="banala", nr_solutii=nSol, timeout=timeout)
-        # print()
-        # graph.a_star(euristica="urmatorul_pas", nr_solutii=nSol, timeout=timeout)
-        # print()
+        print("ucs")
+        graph.ucs(nr_solutii=nSol, timeout=timeout)
+
+        print("a star")
+        print("banala")
+        graph.a_star(euristica="banala", nr_solutii=nSol, timeout=timeout)
+        print("urmatorul_pas")
+        graph.a_star(euristica="urmatorul_pas", nr_solutii=nSol, timeout=timeout)
+        # print("peste 2 pasi")
         # graph.a_star(euristica="peste_2_pasi", nr_solutii=nSol, timeout=timeout)
-        #
-        # print("a star optim")
-        # graph.a_star_optimizat(euristica="banala", timeout=timeout)
-        # print()
-        # graph.a_star_optimizat(euristica="urmatorul_pas", timeout=timeout)
-        # print()
-        # graph.a_star_optimizat(euristica="peste_2_pasi", timeout=timeout)
+
+        print("a star optim")
+        print("banala")
+        graph.a_star_optimizat(euristica="banala", timeout=timeout)
+        print("urmatorul_pas")
+        graph.a_star_optimizat(euristica="urmatorul_pas", timeout=timeout)
+        print("peste 2 pasi")
+        graph.a_star_optimizat(euristica="peste_2_pasi", timeout=timeout)
 
         print("ida star")
+        print("banala")
         graph.ida_star(euristica="banala", timeout=timeout, nr_solutii=nSol)
-        print()
+        print("urmatorul_pas")
         graph.ida_star(euristica="urmatorul_pas", timeout=timeout, nr_solutii=nSol)
-        # print()
+        # print("peste 2 pasi")
         # graph.ida_star(euristica="peste_2_pasi", timeout=timeout)
 
         fin.close()
